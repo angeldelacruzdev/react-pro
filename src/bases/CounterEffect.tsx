debc09193ff4081ab60bda6ad1d0329bb606ac50
Angél De La Cruz <angeldelacruzdev@gmail.com>
2021-12-06 09:15:35 -0400

    Sección: useRef y TimeLines

--- src/bases/CounterEffect.tsx
+++ src/bases/CounterEffect.tsx
@@ -1,10 +1,11 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { gsap } from "gsap";
 
 const MAXIMUN_COUNT = 10;
 
 const CounterEffect = () => {
   const [counter, setCounter] = useState(8);
+  const counterElement = useRef<HTMLHeadingElement>(null);
 
   const handleCounter = () => {
     setCounter((prev) => Math.min(prev + 1, MAXIMUN_COUNT));
@@ -14,15 +15,23 @@ const CounterEffect = () => {
     if (counter < 10) return;
     console.warn("Llego al valor máximo");
 
-    gsap.to("h2", { y: -10, duration: 0.2, ease: "ease.out" }).then(() => {
-      gsap.to("h2", { y: 10, duration: 0.5, ease: "ease.out" });
+    const tl = gsap.timeline();
+
+    tl.to(counterElement.current, {
+      y: -10,
+      duration: 0.2,
+      ease: "ease.out",
+    }).to(counterElement.current, {
+      y: 10,
+      duration: 0.5,
+      ease: "ease.out",
     });
   }, [counter]);
 
   return (
     <div>
       <h1>CounterEffect</h1>
-      <h2> {counter}</h2>
+      <h2 ref={counterElement}> {counter}</h2>
       <button onClick={() => handleCounter()}>+1</button>
     </div>
   );
